7f1228dc2979ada229eac92eb83c45426064399c
2a6m <maxime.bourguignon1996@gmail.com>
2019-05-20 20:28:38 +0200

    mock artist

---
+++ myApp/src/app/mock-artists.ts
@@ -0,0 +1,6 @@
+import { Artist } from './artist';
+
+export const ARTISTS: Artist[] = [
+    { id: 1, name: 'Slash', birthdate: '2019-05-16', genre: 'Rock' },
+    { id: 2, name: 'Selah Sue', birthdate: '2000-12-05', genre: 'Soul' }
+];
