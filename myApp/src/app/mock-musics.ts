bc9f6c06b671127cc0980a94cccc94d1bd3dc2bb
2a6m <maxime.bourguignon1996@gmail.com>
2019-05-20 21:28:22 +0200

    add musics to test

--- myApp/src/app/mock-musics.ts
+++ myApp/src/app/mock-musics.ts
@@ -1,5 +1,6 @@
 import { Music } from './music';
 
 export const MUSICS: Music[] = [
-    { id: 1, title: 'Bad Rain', duration: '12:12', genre: 'Rock', artist: { id: 1, name: 'Slash', birthdate: '2019-05-16', genre: 'Rock' }, date: '2012-12-12'}
+    { id: 1, title: 'Bad Rain', duration: '12:12', genre: 'Rock', artist: { id: 1, name: 'Slash', birthdate: '2019-05-16', genre: 'Rock' }, date: '2012-12-12'},
+    { id: 2, title: 'Alone', duration: '6:66', genre: 'Soul', artist: { id: 2, name: 'Selah Sue', birthdate: '2000-12-05', genre: 'Soul' }, date: '2012-12-12'}
 ];
