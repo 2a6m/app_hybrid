b98963d5cadb3e582c0657133c852b74079ff5f3
2a6m <maxime.bourguignon1996@gmail.com>
2019-05-26 21:05:19 +0200

    create possibility to modify artist

--- myApp/src/app/update-artist/update-artist.page.ts
+++ myApp/src/app/update-artist/update-artist.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Artist } from '../artist';
+import { ArtistService } from '../services/artist.service';
 
 @Component({
   selector: 'app-update-artist',
@@ -6,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update-artist.page.scss'],
 })
 export class UpdateArtistPage implements OnInit {
+    artistId: number;
+    artist: Artist;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+                private artistservice: ArtistService,
+                private router: Router) { }
 
   ngOnInit() {
+      this.artistId = Number(this.route.snapshot.paramMap.get('id'));
+      this.artist = this.artistservice.getArtist(this.artistId);
+      console.log(this.artist);
   }
 
 }
