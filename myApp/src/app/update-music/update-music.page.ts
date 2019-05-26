6ff615b1167a2d4fb671aef7b8ef7e32c5a62103
2a6m <maxime.bourguignon1996@gmail.com>
2019-05-26 21:36:41 +0200

    create page to update music

--- myApp/src/app/update-music/update-music.page.ts
+++ myApp/src/app/update-music/update-music.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Music } from '../music';
+import { MusicService } from '../services/music.service';
+import { Artist } from '../artist';
+import { ArtistService } from '../services/artist.service';
 
 @Component({
   selector: 'app-update-music',
@@ -6,10 +11,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update-music.page.scss'],
 })
 export class UpdateMusicPage implements OnInit {
+    musicId: number;
+    music: Music;
+    artists: Artist[];
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+                private musicservice: MusicService,
+                private artistservice: ArtistService,
+                private router: Router) { }
 
   ngOnInit() {
+      this.artists = this.artistservice.getArtists();
+      this.musicId = Number(this.route.snapshot.paramMap.get('id'));
+      this.music = this.musicservice.getMusic(this.musicId);
+      console.log(this.music);
   }
 
 }
